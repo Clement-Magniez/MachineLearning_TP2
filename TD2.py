5c3aeed2b0e37e2a478d0efb026fd4484d2ccd7d
Clement Magniez <clement.magniez@depinfonancy.net>
2022-10-24 13:07:25 +0200

    cnn setup + first predictions

--- TD2.py
+++ TD2.py
@@ -1,30 +1,44 @@
+from re import X
 import sys
+from time import time
 import torch.nn as nn
 import torch
 import numpy as np
 
-dn = 50.
+dn = 1.
 h=10
-nepochs=100
+nepochs=10
+timeframe=10
 
 with open("2019.csv","r") as f: ls=f.readlines()
-trainx = torch.Tensor([float(l.split(',')[1])/dn for l in ls[:-1]]).view(1,-1,1)
-trainy = torch.Tensor([float(l.split(',')[1])/dn for l in ls[1:]]).view(1,-1,1)
+basetrainx = torch.Tensor([float(l.split(',')[1])/dn for l in ls]).view(1,-1,1)
 with open("2020.csv","r") as f: ls=f.readlines()
-testx = torch.Tensor([float(l.split(',')[1])/dn for l in ls[:-1]]).view(1,-1,1)
-testy = torch.Tensor([float(l.split(',')[1])/dn for l in ls[1:]]).view(1,-1,1)
+basetestx = torch.Tensor([float(l.split(',')[1])/dn for l in ls]).view(1,-1,1)
 
-trainx_mean = torch.mean(trainx)
-trainy_mean = torch.mean(trainy)
-testx_mean = torch.mean(testx)
-testy_mean = torch.mean(testy)
+basetrainx_mean = torch.mean(basetrainx)
+basetestx_mean = torch.mean(basetestx)
 
-trainx_std = torch.std(trainx)
-trainy_std = torch.std(trainy)
-testx_std = torch.std(testx)
-testy_std = torch.std(testy)
+basetrainx_std = torch.std(basetrainx)
+basetestx_std = torch.std(basetestx)
 
-print(trainx_mean, trainy_mean, testx_mean, testy_mean)
+basetrainx = basetrainx - basetrainx_mean / torch.sqrt(basetrainx_std)
+basetestx = basetestx - basetestx_mean / torch.sqrt(basetestx_std)
+
+trainx, trainy, testx, testy = [], [], [], []
+for i in range(365-timeframe-1):
+    trainx.append(basetrainx[0, i:i+timeframe])
+    trainy.append(basetrainx[0, i+timeframe])
+    testx.append(basetestx[0, i:i+timeframe])
+    testy.append(basetestx[0, i+timeframe])
+
+trainx = torch.stack(trainx)
+trainy = torch.stack(trainy)
+testx = torch.stack(testx)
+testy = torch.stack(testy)
+
+print(trainx.shape, trainy.shape, testx.shape, testy.shape)
+
+print(basetrainx_mean, basetestx_mean, basetrainx_std, basetestx_std)
 
 trainds = torch.utils.data.TensorDataset(trainx, trainy)
 trainloader = torch.utils.data.DataLoader(trainds, batch_size=1, shuffle=False)
@@ -32,20 +46,44 @@ testds = torch.utils.data.TensorDataset(testx, testy)
 testloader = torch.utils.data.DataLoader(testds, batch_size=1, shuffle=False)
 crit = nn.MSELoss()
 
-class Mod(nn.Module):
+class ModRnn(nn.Module):
     def __init__(self,nhid):
-        super(Mod, self).__init__()
+        super(ModRnn, self).__init__()
         self.rnn = nn.RNN(1,nhid)
         self.mlp = nn.Linear(nhid,1)
 
     def forward(self,x):
         # x = B, T, d
+        # x = N, L, Hin
         xx = x.transpose(0,1)
+        # x =  L, N, Hin
         y,_=self.rnn(xx)
         T,B,H = y.shape
         y = self.mlp(y.view(T*B,H))
         y = y.view(T,B,-1)
         y = y.transpose(0,1)
+        return y
+
+class ModCnn(nn.Module):
+    def __init__(self, nhid, kernel_size, timeframe):
+        self.nhid = nhid
+        self.kernel_size = kernel_size
+        self.timeframe = timeframe
+
+        super(ModCnn, self).__init__()
+        self.cnn = nn.Conv1d(1, nhid, kernel_size=kernel_size)
+        self.mlp = nn.Linear(nhid*(timeframe-kernel_size+1), 1)
+
+    def forward(self, x):
+        # x = batchsize=1, timeframe=variable, nchannels=1
+        xx = x.transpose(1, 2)
+        # xx = N, Hin, L
+        y = self.cnn(xx)
+        T,B,H = y.shape
+
+
+        y = self.mlp( y.reshape(1*self.nhid*(self.timeframe-self.kernel_size+1)))
+
         return y
 
 def test(mod):
@@ -79,6 +117,15 @@ def train(mod):
         print("err",totloss,testloss)
     print("fin",totloss,testloss,file=sys.stderr)
 
-mod=Mod(h)
-print("nparms",sum(p.numel() for p in mod.parameters() if p.requires_grad),file=sys.stderr)
-train(mod)
+"""
+modRnn=ModRnn(h)
+print("nparms",sum(p.numel() for p in modRnn.parameters() if p.requires_grad),file=sys.stderr)
+train(modRnn)
+"""
+
+modCnn=ModCnn(h, 3, 10)
+print("nparms",sum(p.numel() for p in modCnn.parameters() if p.requires_grad),file=sys.stderr)
+train(modCnn)
+print(testx[:1].shape)
+
+print(f"{modCnn(testx[:1]) * torch.sqrt(basetestx_std) + basetestx_mean}, {testy[:1] * torch.sqrt(basetestx_std) + basetestx_mean}")
